e000977e802fa89063ea77a2dc61246a67a9e83f
Napoleon314 <napoleon356977480@gmail.com>
2015-08-04 02:38:59 +0800

    event

--- Source/VePower/VeEvents.h
+++ Source/VePower/VeEvents.h
@@ -14,3 +14,109 @@
 
 #pragma once
 
+enum VeEventType
+{
+	VE_FIRSTEVENT = 0,
+	VE_QUIT = 0x100,
+	VE_APP_TERMINATING,
+	VE_APP_LOWMEMORY,
+	VE_APP_WILLENTERBACKGROUND,
+	VE_APP_DIDENTERBACKGROUND,
+	VE_APP_WILLENTERFOREGROUND,
+	VE_APP_DIDENTERFOREGROUND,
+
+	VE_WINDOWEVENT = 0x200,
+	VE_SYSWMEVENT,
+
+	VE_KEYDOWN = 0x300,
+	VE_KEYUP,
+	VE_TEXTEDITING,
+	VE_TEXTINPUT,
+
+	VE_MOUSEMOTION = 0x400,
+	VE_MOUSEBUTTONDOWN,
+	VE_MOUSEBUTTONUP,
+	VE_MOUSEWHEEL,
+
+	VE_JOYAXISMOTION = 0x600,
+	VE_JOYBALLMOTION,
+	VE_JOYHATMOTION,
+	VE_JOYBUTTONDOWN,
+	VE_JOYBUTTONUP,
+	VE_JOYDEVICEADDED,
+	VE_JOYDEVICEREMOVED,
+
+	VE_CONTROLLERAXISMOTION = 0x650,
+	VE_CONTROLLERBUTTONDOWN,
+	VE_CONTROLLERBUTTONUP,
+	VE_CONTROLLERDEVICEADDED,
+	VE_CONTROLLERDEVICEREMOVED,
+	VE_CONTROLLERDEVICEREMAPPED,
+
+	VE_FINGERDOWN = 0x700,
+	VE_FINGERUP,
+	VE_FINGERMOTION,
+
+	VE_DOLLARGESTURE = 0x800,
+	VE_DOLLARRECORD,
+	VE_MULTIGESTURE,
+
+	VE_CLIPBOARDUPDATE = 0x900,
+
+	VE_DROPFILE = 0x1000,
+
+	VE_RENDER_TARGETS_RESET = 0x2000,
+
+	VE_USEREVENT = 0x8000,
+
+	VE_LASTEVENT = 0xFFFF
+};
+
+struct VeCommonEvent
+{
+	VeUInt32 m_u32Type;
+	VeUInt32 m_u32TimeStamp;
+};
+
+struct VeWindowEvent
+{
+	VeUInt32 m_u32Type;
+	VeUInt32 m_u32TimeStamp;
+	VeUInt32 m_u32WindowID;
+	VeUInt8 m_u8Event;
+	VeUInt8 m_u8Padding1;
+	VeUInt8 m_u8Padding2;
+	VeUInt8 m_u8Padding3;
+	VeInt32 m_i32Data1;
+	VeInt32 m_i32Data2;
+};
+
+struct VeQuitEvent
+{
+	VeUInt32 m_u32Type;
+	VeUInt32 m_u32TimeStamp;
+};
+
+union VeEvent
+{
+	VeUInt32 m_u32Type;
+	VeCommonEvent m_kCommon;
+	VeWindowEvent m_kWindow;
+	VeQuitEvent m_kQuit;
+
+	VeUInt8 m_au8Padding[56];
+};
+
+class VE_POWER_API VeEventQueue : public VeRefObject
+{
+public:
+	VeEventQueue() noexcept;
+
+	~VeEventQueue() noexcept;
+
+protected:
+
+
+};
+
+VeSmartPointer(VeEventQueue);
