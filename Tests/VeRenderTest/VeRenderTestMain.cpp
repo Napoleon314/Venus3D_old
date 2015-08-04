441ab9e1a0cf3b2012c7f42ac9eeb6c5b55403ed
Napoleon314 <napoleon356977480@gmail.com>
2015-08-04 22:41:25 +0800

    add rendertest

---
+++ Tests/VeRenderTest/VeRenderTestMain.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  Venus Engine Source File.
+//  Copyright (C), Venus Interactive Entertainment.2012
+// -------------------------------------------------------------------------
+//  Module:      VeRenderTest
+//  File name:   VeRenderTestMain.cpp
+//  Created:     2015/08/04 by Napoleon
+//  Description: 
+// -------------------------------------------------------------------------
+//  History:
+//  http://www.venusie.com
+////////////////////////////////////////////////////////////////////////////
+
+#include <VePowerPch.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+{
+	return 0;
+}
