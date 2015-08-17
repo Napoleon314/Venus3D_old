903f92e05348c42a07a3534aadbd232e59fc8333
Napoleon314 <napoleon356977480@gmail.com>
2015-08-18 00:06:57 +0800

    cpuinfo

--- Source/VePower/VeCPUInfo.h
+++ Source/VePower/VeCPUInfo.h
@@ -21,7 +21,33 @@ class VE_POWER_API VeCPUInfo : public VeMemObject
 public:
 	static VeInt32 GetCount() noexcept;
 
+	static VeInt32 GetCacheLineSize() noexcept;
+
+	static bool HasRDTSC() noexcept;
+
+	static bool HasMMX() noexcept;
+
+	static bool Has3DNow() noexcept;
+
+	static bool HasSSE() noexcept;
+
+	static bool HasSSE2() noexcept;
+
+	static bool HasSSE3() noexcept;
+
+	static bool HasSSE41() noexcept;
+
+	static bool HasSSE42() noexcept;
+
+	static bool HasAVX() noexcept;
+
+	static VeInt32 GetSystemRAM() noexcept;
+
 protected:
+	static VeUInt32 GetCPUFeatures() noexcept;
+
 	static VeInt32 ms_i32CPUCount;
+	static VeInt32 ms_i32CPUFeatures;
+	static VeInt32 ms_i32SystemRAM;
 
 };
