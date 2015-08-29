6ffcac95561843eebc811d42cf5833a8b18eec6d
Napoleon314 <napoleon356977480@gmail.com>
2015-08-29 23:04:15 +0800

    delete d3dx

--- Source/VeMain/D3D12/VeRendererWindowD3D12.h
+++ Source/VeMain/D3D12/VeRendererWindowD3D12.h
@@ -12,56 +12,6 @@
 //  http://www.venusie.com
 ////////////////////////////////////////////////////////////////////////////
 
-struct CD3DX12_RESOURCE_BARRIER : public D3D12_RESOURCE_BARRIER
-{
-	CD3DX12_RESOURCE_BARRIER()
-	{}
-	explicit CD3DX12_RESOURCE_BARRIER(const D3D12_RESOURCE_BARRIER &o) :
-		D3D12_RESOURCE_BARRIER(o)
-	{}
-	static inline CD3DX12_RESOURCE_BARRIER Transition(
-		_In_ ID3D12Resource* pResource,
-		D3D12_RESOURCE_STATES stateBefore,
-		D3D12_RESOURCE_STATES stateAfter,
-		UINT subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
-		D3D12_RESOURCE_BARRIER_FLAGS flags = D3D12_RESOURCE_BARRIER_FLAG_NONE)
-	{
-		CD3DX12_RESOURCE_BARRIER result;
-		ZeroMemory(&result, sizeof(result));
-		D3D12_RESOURCE_BARRIER &barrier = result;
-		result.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-		result.Flags = flags;
-		barrier.Transition.pResource = pResource;
-		barrier.Transition.StateBefore = stateBefore;
-		barrier.Transition.StateAfter = stateAfter;
-		barrier.Transition.Subresource = subresource;
-		return result;
-	}
-	static inline CD3DX12_RESOURCE_BARRIER Aliasing(
-		_In_ ID3D12Resource* pResourceBefore,
-		_In_ ID3D12Resource* pResourceAfter)
-	{
-		CD3DX12_RESOURCE_BARRIER result;
-		ZeroMemory(&result, sizeof(result));
-		D3D12_RESOURCE_BARRIER &barrier = result;
-		result.Type = D3D12_RESOURCE_BARRIER_TYPE_ALIASING;
-		barrier.Aliasing.pResourceBefore = pResourceBefore;
-		barrier.Aliasing.pResourceAfter = pResourceAfter;
-		return result;
-	}
-	static inline CD3DX12_RESOURCE_BARRIER UAV(
-		_In_ ID3D12Resource* pResource)
-	{
-		CD3DX12_RESOURCE_BARRIER result;
-		ZeroMemory(&result, sizeof(result));
-		D3D12_RESOURCE_BARRIER &barrier = result;
-		result.Type = D3D12_RESOURCE_BARRIER_TYPE_UAV;
-		barrier.UAV.pResource = pResource;
-		return result;
-	}
-	operator const D3D12_RESOURCE_BARRIER&() const { return *this; }
-};
-
 class VeRenderWindowD3D12 : public VeRenderWindow
 {
 	VeNoCopy(VeRenderWindowD3D12);
