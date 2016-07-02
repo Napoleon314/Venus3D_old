d26e7d8e1c0d6b952d63d93496ac45dc70e21cb3
Albert D Yang <Napoleon356977480@gmail.com>
2016-07-03 05:06:01 +1000

    change main fun

--- Test/PowerTest/Main.cpp
+++ Test/PowerTest/Main.cpp
@@ -31,7 +31,7 @@
 #include <Venus3D.h>
 //#include <Math/DirectXMath.h>
 
-int main(int argc, char * argv[])
+int main(/*int argc, char * argv[]*/)
 {
 	printf("CPU: %d\n", VeGetCPUCount());
 	return 0;
